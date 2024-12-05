1960a3e0110511548d96a56643dfae55138b28ec
money <202220182@tesco.edu.mx>
2024-12-05 12:42:46 -0600

    add-montse

--- src/data.js
+++ src/data.js
@@ -4,5 +4,11 @@ export const students = [
     "lastname": "Jurado",
     "favorite_subject": "POO",
     "whatever": "Siempre da lo mejor de ti"
-  },  
+  }, 
+  {
+    "name": "Montse",
+    "lastname": "Flores",
+    "favorite_subject": "sistemas oerativos",
+    "whatever": "Me gusto mucho el curso jiji :D"
+  }, 
 ];
