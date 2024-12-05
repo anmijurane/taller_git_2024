956439cea8797652bcd0c98324de8027397d9c8f
MK1178 <ssscorpion777@gmail.com>
2024-12-05 13:13:59 -0600

    [ADD-Milcun] data.js

--- src/data.js
+++ src/data.js
@@ -11,4 +11,10 @@ export const students = [
     "favorite_subject": "sistemas oerativos",
     "whatever": "Me gusto mucho el curso jiji :D"
   }, 
+  {
+    "name": "Erick",
+    "lastname": "Guerrero",
+    "favorite_subject": "Inteligencia Artificial",
+    "whatever": "Confiar es bueno"
+  },  
 ];
