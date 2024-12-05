382f04d881ee224ac8ced484bdb56fed23aa0c5c
Kek <browis7306@gmail.com>
2024-12-05 11:44:25 -0700

    kev-card

--- src/data.js
+++ src/data.js
@@ -3,6 +3,12 @@ export const students = [
     "name": "Andres",
     "lastname": "Jurado",
     "favorite_subject": "POO",
-    "whatever": "Siempre da lo mejor de ti"
-  },  
+    "whatever": "Siempre da lo mejor de ti",
+  },
+  {
+    "name": "Kevin",
+    "lastname": "Flores",
+    "favorite_subject": "Fundamentos de programacion",
+    "whatever": "Vean himym"
+  }
 ];
